1606e7203d4d8c3c5caef23e8cb1e3d7546559ce
kavindu <kavindug@hsenidmobile.com>
2024-02-08 11:05:59 +0530

    Add active mq producer sample application

---
+++ active-mq-producer/src/test/java/org/example/activemqproducer/ActiveMqProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.activemqproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActiveMqProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
